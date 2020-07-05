8cae2783634f119d73889a45f6cbc7f4fd78a5ce
fangcun <1821075638@qq.com>
2020-07-05 13:48:32 +0800

    新增rsa des base64

--- app/src/main/java/com/xly/ylk/MainActivity.java
+++ app/src/main/java/com/xly/ylk/MainActivity.java
@@ -3,8 +3,15 @@ package com.xly.ylk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.xly.encryptutils.AesUtil;
+import com.xly.encryptutils.BaseUtil;
+import com.xly.encryptutils.DesUtil;
+import com.xly.encryptutils.RsaUtil;
+
+import java.io.UnsupportedEncodingException;
+import java.security.KeyPair;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +21,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        String key = "xuexi.iefeel.com";
+        String key = "12345678";
         //加密
         String encsome = AesUtil.encrypt(key, "要加密的内容");
         //解密
         String decsome = AesUtil.decrypt(key, "要解密的内容");
 
+        String dess = DesUtil.encryptDES("我喜欢你11511515151561515151511515121521521521",key);
+
+        String a = "caonimalegebi";
+        String bs = BaseUtil.encode(a.getBytes());
+        byte[] rs = BaseUtil.decode(bs);
+
+
+        KeyPair keys = RsaUtil.generateRSAKeyPair();
+        Log.d("ttt",BaseUtil.encode(keys.getPublic().getEncoded()));
     }
 }
